ec926796f4364cb9ddc882e733ea7dd09ddd67f5
m5623skhj <m5623skhj@gmail.com>
2021-08-30 21:31:37 +0900

    take 1

--- JenkinsBuildTestProject/Program.cs
+++ JenkinsBuildTestProject/Program.cs
@@ -7,6 +7,7 @@ namespace JenkinsBuildTestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("take 1");
         }
     }
 }
